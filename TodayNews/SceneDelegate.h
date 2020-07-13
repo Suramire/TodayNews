e5775aa4f1a0aa0a2b0a661325dc8ef4265eaa09
Suramire <suramire@126.com>
2020-07-13 10:12:21 +0800

    init

---
+++ TodayNews/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  TodayNews
+//
+//  Created by Suramire on 2020/7/13.
+//  Copyright © 2020 Suramire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ TodayNews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TodayNews
+//
+//  Created by Suramire on 2020/7/13.
+//  Copyright © 2020 Suramire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
